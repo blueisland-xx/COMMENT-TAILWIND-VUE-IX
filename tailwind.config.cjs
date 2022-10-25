7443504f54c14d7fcaadba5be1059a6e4d01ab91
blueisland <349093474@qq.com>
2022-10-25 16:52:38 +0800

    这是部署前的代码

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./index.html"],
+  mode:"jit",
+  //优化： 可以扫描到src下所有.vue .js文件 对其中的css进行处理
+  purge: ["./index.html","./src/**/*.{vue,js}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
